56fcfb1be05c772f08e9c72cc02d657b5a195601
tumana777 <tumana991@gmail.com>
2022-12-09 12:32:06 +0400

    finish game

--- app.js
+++ app.js
@@ -6,86 +6,91 @@ const hold = document.querySelector('.btn--hold');
 const imgDice = document.querySelector('.dice');
 const player1 = document.querySelector('.player--0');
 const player2 = document.querySelector('.player--1');
-const player1scoreEl = document.getElementById('score--0');
-const player2scoreEl = document.getElementById('score--1');
 const player1currentEl = document.getElementById('current--0');
 const player2currentEl = document.getElementById('current--1');
+const player1totalEl = document.getElementById('score--0');
+const player2totalEl = document.getElementById('score--1');
 
-let player1score = 0;
-let player2score = 0;
 let player1current = 0;
 let player2current = 0;
-
-let dice;
+let player1total = 0;
+let player2total = 0;
 
 rollDice.addEventListener('click', () => {
-    dice = Math.ceil(Math.random() * 6);
+    const dice = Math.ceil(Math.random() * 6);
+    imgDice.classList.remove('hidden');
+    imgDice.src = `img/dice-${dice}.png`;
 
     if (player1.classList.contains('player--active')) {
-        player1score += dice;
-        player1scoreEl.textContent = player1score;
+        player1current += dice;
+        player1currentEl.textContent = player1current;
     } else {
-        player2score += dice;
-        player2scoreEl.textContent = player2score;
+        player2current += dice;
+        player2currentEl.textContent = player2current;
     }
 
     if (dice === 1) {
-        imgDice.src = 'img/dice-1.png';
         if (player1.classList.contains('player--active')) {
-            player1score = 0;
-            player1scoreEl.textContent = player1score;
-            player1.classList.remove('player--active');
-            player2.classList.add('player--active');
+            player1current = 0;
+            player1currentEl.textContent = player1current;
+            switchPlayer2();
         } else {
-            player2score = 0;
-            player2scoreEl.textContent = player2score;
-            player1.classList.add('player--active');
-            player2.classList.remove('player--active');
+            player2current = 0;
+            player2currentEl.textContent = player2current;
+            switchPlayer1();
         }
-    } else if (dice === 2) {
-        imgDice.src = 'img/dice-2.png'
-    } else if (dice === 3) {
-        imgDice.src = 'img/dice-3.png'
-    } else if (dice === 4) {
-        imgDice.src = 'img/dice-4.png'
-    } else if (dice === 5) {
-        imgDice.src = 'img/dice-5.png'
-    } else if (dice === 6) {
-        imgDice.src = 'img/dice-6.png'
     }
-
-    
-
 });
 
 hold.addEventListener('click', () => {
     if (player1.classList.contains('player--active')) {
-        player1current += player1score;
+        player1total += player1current;
+        player1totalEl.textContent = player1total;
+        player1current = 0;
         player1currentEl.textContent = player1current;
-        player1score = 0;
-        player1scoreEl.textContent = player1score;
-        player1.classList.remove('player--active');
-        player2.classList.add('player--active');
+        switchPlayer2();
+        if (player1total >= 100) {
+            player1.classList.add('player--winner');
+            rollDice.disabled = true;
+            hold.disabled = true;
+        };
     } else {
-        player2current += player2score;
+        player2total += player2current;
+        player2totalEl.textContent = player2total;
+        player2current = 0;
         player2currentEl.textContent = player2current;
-        player2score = 0;
-        player2scoreEl.textContent = player2score;
-        player1.classList.add('player--active');
-        player2.classList.remove('player--active');
+        switchPlayer1();
+        if (player2total >= 100) {
+            player2.classList.add('player--winner');
+            rollDice.disabled = true;
+            hold.disabled = true;
+        }
     }
 });
 
 newGame.addEventListener('click', () => {
-    player1score = 0;
     player1current = 0;
-    player2score = 0;
+    player1total = 0;
     player2current = 0;
-    player1scoreEl.textContent = player1score;
+    player2total = 0;
     player1currentEl.textContent = player1current;
-    player2scoreEl.textContent = player2score;
+    player1totalEl.textContent = player1total;
     player2currentEl.textContent = player2current;
+    player2totalEl.textContent = player2total;
+    switchPlayer1();
+    imgDice.classList.add('hidden');
+    player1.classList.remove('player--winner');
+    player2.classList.remove('player--winner');
+    rollDice.disabled = false;
+    hold.disabled = false;
+})
+
+function switchPlayer2() {
+    player1.classList.remove('player--active');
+    player2.classList.add('player--active');
+}
+
+function switchPlayer1() {
     player1.classList.add('player--active');
     player2.classList.remove('player--active');
-    imgDice.src = 'img/dice-5.png';
-})
+}
